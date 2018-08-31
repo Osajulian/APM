a65f41292f605e39c168e09c1b3278f22b78ce12
Osajulian <you@example.com>
2018-08-31 08:33:13 -0400

    Passing Parameters to a Route

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: String = 'Product Detail';
+  product: IProduct;
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += ` ${id}`;
+
+    // we hardcode  aproduct to test the id routing.
+    // In a real world app, you'd write the code here to fetch data using http
+    this.product = {
+'productId': id,
+'productName': 'Leaf Lake',
+'productCode': 'GDN-0011',
+'releaseDate': 'March 19, 2016',
+'description': 'Leaf rake with 48-inch wooden handle',
+'price': 19.95,
+'starRating': 3.2,
+'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
   }
 
 }
